c4d34cc09eb9101c4942866d439802686b937352
arestkid <87364498+arestkid@users.noreply.github.com>
2021-07-14 19:32:05 +0300

    Update yandexBot.js

--- yandexBot.js
+++ yandexBot.js
@@ -5,16 +5,51 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
-// @icon         
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
-if(BtnK!=undefined){
+
 let yandexInput = document.getElementById("text");
-yandexInput.value= "как звучит Гобой";
+let btnK = document.getElementsByClassName("button mini-suggest__button")[0];
+
+if(btnK != undefined){
+    yandexInput.value= "как звучит Гобой";
+    setTimeout(function(){
+        btnK.click();// Клик по кнопке поиска
+    }, 1000);
+}else if(location.hostname === "https://yandex.ru/"){
+    let links = document.links;
+    let goNext = true;
+    for(let i=0; i<links.length; i++){
+        let link = links[i];
+        if(link.href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+            setTimeout(function(){
+                link.click();
+            }, 3000);
+            goNext = false;
+            break;
+        }
+    }
+    if(goNext){
+        let btnext = document.getElementsByClassName("link link_theme_none link_target_serp pager__item pager__item_kind_next i-bem")[0];
+        setTimeout(function(){
+            btnext.click();
+        }, 3000);
+    }
 }else{
     let links = document.links;
-    for(let i=0;i<links.length;i++){
-        let links = links[i];
-        if
-let BtnK = document.getElementsByClassName("button mini-suggest__button")[0];
-BtnK.click();
+    let randomIndex = getIntRandom(0, links.length);
+    let link = links[randomIndex];
+    if(link.href.indexOf(location.hostname) != -1){
+        setTimeout(function(){
+            links[randomIndex].click();
+        }, 2000);
+    }else{
+        location.href = "https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/";
+    }
+}
+
+function getIntRandom(min, max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
